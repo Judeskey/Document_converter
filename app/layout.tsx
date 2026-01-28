3975a6474e74946fa22874337b22090d4c43ee52
Jude Okoye <judeskey2002@gmail.com>
2026-01-28 01:32:15 -0500

    Add GA4 global site tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SiteHeader from "@/components/SiteHeader";
 import AuthButtons from "@/components/AuthButtons";
 import GoProButton from "@/components/GoProButton";
 import SiteFooter from "@/components/SiteFooter";
+import Script from "next/script";
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://docconvertor.com";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,6 +52,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          send_page_view: false
+        });
+      `}
+          </Script>
+        </>
+      )}
+
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
